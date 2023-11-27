82793c8fd01e9ae516d82d41ff2978ffb45a9d0e
Tian2803 <sebastianburgosperez82@gmail.com>
2023-11-26 21:22:50 -0500

    ultimos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Notify home',
       theme: ThemeData(
         // Definición del esquema de colores de la aplicación
